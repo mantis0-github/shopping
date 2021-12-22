8d2e1971c921fac7e9158709cb231dbc8eff1e5e
Ninhlx <ninhlxph09540@fpt.edu.vn>
2021-12-22 16:46:56 +0700

    BV the first

--- src/main/java/com/project/shopping/service/IRolesService.java
+++
@@ -1,7 +0,0 @@
-package com.project.shopping.service;
-
-import com.project.shopping.model.Roles;
-
-public interface IRolesService extends IGeneralService<Roles> {
-    Roles findByRole(String role);
-}
--- src/main/java/com/project/shopping/service/IUsersService.java
+++
@@ -1,6 +0,0 @@
-package com.project.shopping.service;
-
-import com.project.shopping.model.Users;
-
-public interface IUsersService extends IGeneralService<Users> {
-}
